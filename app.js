09d6072501aebbd6065ba6b87be2c25bc2b30a83
pk prajapati <pappu.kumar@fluper.in>
2022-12-04 23:10:11 +0530

    today code over

--- app.js
+++ app.js
@@ -1,43 +1,48 @@
-var createError     = require('http-errors');
-var express         = require('express');
-var path            = require('path');
-var cookieParser    = require('cookie-parser');
-var logger          = require('morgan');
-var autocannon      = require('autocannon')
-var starsRouter     = require('./routes/star');
-var indexRouter     = require('./routes/index');
-var usersRouter     = require('./routes/users');
-var adminRouter     = require('./routes/admin');
-var visionRouter    = require('./routes/vision');
-var galaxyRouter    = require('./routes/galaxy');
-var planetRouter    = require('./routes/planet');
-var publishRouter   = require('./routes/publish');
-var compression     = require('compression')
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var autocannon = require('autocannon')
+var starsRouter = require('./routes/star');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var adminRouter = require('./routes/admin');
+var visionRouter = require('./routes/vision');
+var galaxyRouter = require('./routes/galaxy');
+var planetRouter = require('./routes/planet');
+var publishRouter = require('./routes/publish');
+var compression = require('compression');
+var rateLimit = require('express-rate-limit')
 
-var app = express();
-app.use(compression(
-    { 
-        level:6,
-        threshold:100*100,
-        filter: shouldCompress,
-    }
-));
-function shouldCompress (req, res) {
+const limiter = rateLimit({
+    max: 200,
+    windowMs: 60 * 60 * 1000,
+    message: "Too many request from this IP"
+});
+
+function shouldCompress(req, res) {
     if (req.headers['x-no-compression']) {
-      // don't compress responses with this request header
-      return false
-    }  
+        // don't compress responses with this request header
+        return false
+    }
     // fallback to standard filter function
     return compression.filter(req, res)
 }
 
 
-
-
+var app = express();
+app.use(limiter);
+app.use(compression(
+    {
+        level: 6,
+        threshold: 100 * 100,
+        filter: shouldCompress,
+    }
+));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -54,19 +59,19 @@ app.use('/vision', visionRouter);
 app.use('/publish', publishRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  next(createError(404));
+app.use(function (req, res, next) {
+    next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+app.use(function (err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 
